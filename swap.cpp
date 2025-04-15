7f19fc10865a390801eb35554bcea740985bff7e
prodhanm <mustafizur.prodhan@gmail.com>
2025-04-15 11:51:20 -0400

    The code is straightforward for cpp and works

--- swap.cpp
+++ swap.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <iostream>  
 
-int main()
+using namespace std;  
+
+int swap(int& a, int& b)
 {
-    std::cout << "Hello World!\n";
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	return a, b;
 }
 
+void main() {  
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	swap(a, b);
+	cout << "After swapping: a = " << a << ", b = " << b << endl;
+}
